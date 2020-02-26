48b77684eec6b3d037291eba314229bfaf0b26c0
Vladimir <velogon6565@gmail.com>
2020-02-26 13:55:09 +0200

    26/02/20 v2.2.1

---
+++ js/test.js
@@ -0,0 +1,69 @@
+'use strict'
+
+
+// alert ("Hello world");
+
+// let vova = {
+//     name: "Vova",
+//     age: 26,
+//     isMarried: true
+// };
+
+// console.log(vova["name"]);
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let arr = ["1.png", "2.jpeg", "3.svg"];
+
+// let answer = prompt("Есть ли тебе 18?", "Да");
+// console.log(typeof(null));
+// console.log("arr" + "asd");
+// console.log(4 + +"6");
+
+// let incr = 10,
+//     decr = 10;
+
+
+// console.log(++incr);
+// console.log(--decr);
+
+// let isChaked = true,
+//     isClose = true;
+
+// console.log(isChaked && isClose);
+
+// if (1) {
+//     console.log("Верно!")
+// } else {
+//     console.log("Не верно!")
+// }
+
+// let num = 50;
+// (num == 12) ? console.log("yese") : console.log("no");
+// if (num < 49) {
+//     console.log("OK")
+// } else if (num > 100) {
+//     console.log("NO")
+// } else {
+//     console.log("YES")
+// };
+
+// switch (num) {
+//     case num < 49:
+//         console.log("No");
+//         break;
+//     case num > 100:
+//         console.log("Nain");
+//         break;
+//     case num > 80:
+//         console.log("Net");
+//         break;
+//     case 50:
+//         console.log("Yes");
+//         break;
+//     default:
+//         console.log("Что-то пошло не так");
+//         break;
+// }
+
+
