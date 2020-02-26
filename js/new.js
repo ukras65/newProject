9f13dc9b6d4c22a13a27f8b3ffd007c1d056d9a3
Vladimir <velogon6565@gmail.com>
2020-02-26 12:03:21 +0200

    This new project 26/02/20

---
+++ js/new.js
@@ -0,0 +1,33 @@
+'use strict'
+
+
+// alert ("Hello world");
+
+// let vova = {
+//     name: "Vova",
+//     age: 26,
+//     isMarried: true
+// };
+
+// console.log(vova["name"]);
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let arr = ["1.png", "2.jpeg", "3.svg"];
+
+// let answer = prompt("Есть ли тебе 18?", "Да");
+// console.log(typeof(null));
+// console.log("arr" + "asd");
+// console.log(4 + +"6");
+
+// let incr = 10,
+//     decr = 10;
+
+
+// console.log(++incr);
+// console.log(--decr);
+
+let isChaked = true,
+    isClose = true;
+
+console.log(isChaked && isClose);
