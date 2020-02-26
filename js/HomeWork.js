95102014952e48b89e7d461f21a8da0955f2222f
Vladimir <velogon6565@gmail.com>
2020-02-26 14:41:00 +0200

    26/02/20 v2.3

---
+++ js/HomeWork.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let money = +prompt("Ваш бюджет на месяц",""),
+    time = +prompt("Введите дату в формате YYYY-MM-DD", "");
+let onequestions = prompt("Введие обязательную статью расходов в этом месяце", "");
+let twoquestion = +prompt("Восколько обойдеться?","");
+
+let appData = {
+    Budjet: money,
+    Time: time,
+    Expenses: {
+        firstQuest: onequestions,
+        twoQuest: twoquestion
+    },
+    optionalExpenses: "",
+    income: "",
+    savings: false
+};
+
+alert((money) / 30);
+
+console.log(appData["Budjet"]);
+console.log(appData["Time"]);
+console.log(appData["Expenses"]);
+console.log(appData["savings"]);
+console.log(money);
+console.log(time);
