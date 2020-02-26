54a5fc0bb813cf88c0855629df590a328f34f9fa
Vladimir <velogon6565@gmail.com>
2020-02-26 13:52:11 +0200

    26/02/20 v 2.2

--- js/new.js
+++ js/new.js
@@ -32,3 +32,38 @@
 
 // console.log(isChaked && isClose);
 
+// if (1) {
+//     console.log("Верно!")
+// } else {
+//     console.log("Не верно!")
+// }
+
+// let num = 50;
+// (num == 12) ? console.log("yese") : console.log("no");
+// if (num < 49) {
+//     console.log("OK")
+// } else if (num > 100) {
+//     console.log("NO")
+// } else {
+//     console.log("YES")
+// };
+
+// switch (num) {
+//     case num < 49:
+//         console.log("No");
+//         break;
+//     case num > 100:
+//         console.log("Nain");
+//         break;
+//     case num > 80:
+//         console.log("Net");
+//         break;
+//     case 50:
+//         console.log("Yes");
+//         break;
+//     default:
+//         console.log("Что-то пошло не так");
+//         break;
+// }
+
+
