a8dbd3869ffdbb42011f6610a9f48565b0b40dfe
Vladimir <velogon6565@gmail.com>
2020-02-26 16:11:26 +0200

    v 2.3.3

--- js/HomeWork.js
+++ js/HomeWork.js
@@ -1,27 +1,55 @@
 "use strict"
 
 let money = +prompt("Ваш бюджет на месяц",""),
-    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
-    onequestions = prompt("Введие обязательную статью расходов в этом месяце", ""),
-    twoquestion = +prompt("Восколько обойдеться?","");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+    
 
 let appData = {
     Budjet: money,
     Time: time,
     Expenses: {
-        firstQuest: onequestions,
-        twoQuest: twoquestion
     },
-    optionalExpenses: "",
-    income: "",
+    optionalExpenses: {},
+    income: [],
     savings: false
 };
 
-alert((money) / 30);
+for (let i = 1; i < 2; i++) {
+    let a = prompt("Введие обязательную статью расходов в этом месяце", ""),
+        b = prompt("Восколько обойдеться?","");
+    appData.Expenses[a] = b;
+    if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null && a !="" && b !="" && a.length < 50) {
+        console.log("Проверка пройдена")
+        appData.Expenses[a] = b; 
+    } else {
 
-console.log(appData["Budjet"]);
-console.log(appData["Time"]);
-console.log(appData["Expenses"]);
-console.log(appData["savings"]);
-console.log(money);
-console.log(time);
+    }
+};
+
+appData.moneyPerDay = appData.Budjet / 30;
+alert(" Ежедневный бюджет :" + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ) {
+    console.log("Cредний достаток");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Ошибка");
+}
+
+console.log(appData);
+
+
+
+
+
+
+
+// console.log(appData["Budjet"]);
+// console.log(appData["Time"]);
+// console.log(appData["Expenses"]);
+// console.log(appData["savings"]);
+// console.log(money);
+// console.log(time);
--- js/test.js
+++ js/test.js
@@ -103,3 +103,11 @@ for (num; num < 55; num++) {
     console.log(num);
 } */
 
+// let x = 5; 
+// console.log(x++);
+
+// console.log([] + false - null + true);
+
+// let y = 1; 
+// let x = y = 2;
+// console.log(2 && 1 && null && 0 && undefined );
